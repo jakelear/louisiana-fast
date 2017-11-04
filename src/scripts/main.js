debf28e5d642204685388592c2f88da025670bbd
Jake Lear <jake.lear@voxmedia.com>
2017-11-04 18:50:14 -0400

    initial functionality working

---
+++ src/scripts/main.js
@@ -0,0 +1,224 @@
+import { get, showElement, hideElement } from "./utils";
+import mapboxgl from "mapbox-gl";
+const turf = require("@turf/turf");
+
+mapboxgl.accessToken = "";
+
+var locations = {};
+var map;
+
+const button = document.getElementById("chicken-btn");
+button.addEventListener("click", function(event) {
+  event.preventDefault();
+  start();
+});
+
+/**
+* getUserLocation
+* - Gets the user's current location using browser geolocation
+*/
+function getUserLocation() {
+  return new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+function init() {
+  const phraseOne = document.getElementById("phrase-one");
+  const phraseTwo = document.getElementById("phrase-two");
+  const buttonLoader = document.getElementById("btn-loader");
+  setTimeout(function() {
+    phraseOne.classList.add("fade-in");
+  }, 200);
+  setTimeout(function() {
+    phraseTwo.classList.add("fade-in");
+  }, 700);
+
+  setTimeout(function() {
+    showElement(buttonLoader, "block");
+    getUserLocation()
+      .then(position => {
+        hideElement(buttonLoader);
+        locations.user = {};
+        locations.user.long = position.coords.longitude;
+        locations.user.lat = position.coords.latitude;
+        locations.user.coords = [locations.user.long, locations.user.lat];
+        showStartButton();
+      })
+      .catch(err => {
+        console.log(err.message);
+      });
+  }, 1200);
+
+  function showStartButton() {
+    const buttonFader = document.getElementById("btn-fader");
+    buttonFader.classList.add("fade-in");
+  }
+}
+
+/**
+* start
+* Runs when user presses the "Find the chicken" button
+* - Uses the mapbox geocoding api to search for nearby Popeyes locations
+* - Limited by proximity to the user's location
+* -
+* -
+*/
+function start() {
+  let landingSection = document.getElementById("landing");
+  let loadingSection = document.getElementById("loading");
+  let resultsSection = document.getElementById("results");
+
+  hideElement(landingSection);
+  showElement(loadingSection, "block");
+
+  let rootGeocodingUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/`;
+  let searchTerm = `Popeyes Louisiana Kitchen`;
+  let localGeocodingSearchUrl = `${rootGeocodingUrl}${searchTerm}.json?access_token=${mapboxgl.accessToken}&country=us&proximity=${locations
+    .user.long}%2C%20${locations.user.lat}`;
+
+  // Fetch the nearby Popeyes locations
+  get(localGeocodingSearchUrl).then(
+    function(response) {
+      // Parse the response and store the features
+      let nearbyLocations = JSON.parse(response).features;
+      let nearestLocation = findNearest(locations.user.coords, nearbyLocations);
+      let mapContainer = document.getElementById("map");
+
+      // Show the results section before appending the map
+      // So mapbox can append properly sized canvas
+      hideElement(loadingSection);
+      showElement(resultsSection, "flex");
+
+      map = new mapboxgl.Map({
+        container: "map",
+        style: "mapbox://styles/mapbox/light-v9",
+        center: locations.user.coords,
+        zoom: 14
+      });
+
+      // create a HTML element for user and location markers
+      var userMarker = document.createElement("div");
+      userMarker.className = "marker";
+
+      var locMarker = document.createElement("div");
+      locMarker.className = "marker";
+      locMarker.classList.add("red");
+
+      // make a marker for each feature and add to the map
+      new mapboxgl.Marker(userMarker)
+        .setLngLat(locations.user.coords)
+        .addTo(map);
+      new mapboxgl.Marker(locMarker)
+        .setLngLat(nearestLocation.geometry.coordinates)
+        .addTo(map);
+
+      let resultsPhraseOne = document.getElementById("results-phrase-one");
+      let resultsPhraseTwo = document.getElementById("results-phrase-two");
+      setTimeout(function() {
+        resultsPhraseOne.classList.add("fade-in");
+      }, 400);
+      setTimeout(function() {
+        resultsPhraseOne.classList.remove("fade-in");
+        resultsPhraseOne.classList.add("fade-out");
+        resultsPhraseTwo.classList.add("fade-in");
+        flyToLocation(nearestLocation);
+        initDirections(nearestLocation);
+      }, 2300);
+    },
+    function(error) {
+      console.error("Failed!", error);
+    }
+  );
+
+  function flyToLocation(location) {
+    map.flyTo({
+      center: location.geometry.coordinates,
+      zoom: 12,
+      speed: 0.6, // make the flying slow
+      curve: 1 // change the speed at which it zooms out
+    });
+  }
+}
+
+function initDirections(destination) {
+  let rootDirectionsUrl = `https://api.mapbox.com/directions/v5/mapbox/driving/`;
+  let mode = "driving";
+  let coordinates = `${locations.user.long}%2C${locations.user
+    .lat}%3B${destination.geometry.coordinates[0]}%2C${destination.geometry
+    .coordinates[1]}`;
+  let directionsUrl = `${rootDirectionsUrl}${coordinates}.json?access_token=${mapboxgl.accessToken}&steps=true`;
+  get(directionsUrl).then(function(response) {
+    console.log(JSON.parse(response));
+    renderDirections(JSON.parse(response));
+  });
+}
+
+function renderDirections(data) {
+  const directionsList = document.getElementById("directions-list");
+  let distance = metersToFixedMiles(data.routes[0].distance);
+  let time = secondsToRoundedMinutes(data.routes[0].duration);
+  console.log(`${distance} miles away.`);
+  console.log(`${time} minutes away`);
+  let directions = data.routes[0].legs[0].steps;
+  for (var i = 0, len = directions.length; i < len; i++) {
+    let template = `
+      <li class="m-directions__item">
+        <span class="m-directions__instruction">${directions[i].maneuver
+          .instruction}</span>
+        <span class="m-directions__distance">${metersToFixedMiles(
+          directions[i].distance
+        )}</span>
+      </li>
+    `;
+    let item = document.createElement("li");
+    item.innerHTML = template;
+    directionsList.appendChild(item);
+  }
+}
+
+/***
+* findNearest
+* Takes the user location and an array of other locations
+* Iterates over the array of locations, measuring the distance
+* then sorting the array by distance and returning the nearest
+*/
+function findNearest(userLocation, searchLocations) {
+  // From: https://www.mapbox.com/help/geocode-and-sort-stores/#find-distance-from-all-locations
+  searchLocations.forEach(function(result) {
+    // Add a distance property to each result
+    // that contains the distance from the user browser location
+    Object.defineProperty(result.properties, "distance", {
+      value: turf.distance(locations.user.coords, result.geometry, "miles"),
+      writable: true,
+      enumerable: true,
+      configurable: true
+    });
+  });
+
+  // Sort the locations by distance
+  searchLocations.sort(function(a, b) {
+    if (a.properties.distance > b.properties.distance) {
+      return 1;
+    }
+    if (a.properties.distance < b.properties.distance) {
+      return -1;
+    }
+    return 0;
+  });
+
+  // Return the closest location by distance
+  // TODO: It might be the case that the closest by distance is not the fastest,
+  // so it could be worthwhile to measure travel time on the N closest locations
+  return searchLocations[0];
+}
+
+function metersToFixedMiles(m) {
+  return (m * 0.000621371192).toFixed(2);
+}
+
+function secondsToRoundedMinutes(s) {
+  return Math.round(s / 60);
+}
+
+init();
